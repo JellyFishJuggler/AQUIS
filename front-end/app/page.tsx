dab50e8f9c861efb95a9d6f5478fbb6505f0c05a
JellyFishJuggler <guptasrijan85@gmail.com>
2026-06-11 22:10:22 +0530

    Next configuration

---
+++ front-end/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to the Front-End!</h1>
+      <p className="mt-4 text-lg">This is the home page of the front-end application.</p>
+    </div>
+  );
+}
